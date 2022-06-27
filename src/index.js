c438812d9c616677f0b762ebe8125aa6b034b5bb
Artem Rozhko <artem1996ecm@mail.ru>
2022-06-27 22:55:41 +0300

    fix:solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  const str = String(n).split("");
+  while (str[str.length - 1] == 0) {
+    str.pop();
+  }
+  if (str[0] == '-') {
+    str.shift();
+  }
+  return Number(str.reverse().join(""));
 }
